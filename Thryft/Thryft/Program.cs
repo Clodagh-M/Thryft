8f979309dfc1588d935751478950792c0cbb22ec
Student Clodagh Mulvihill (TL_KCOMP_B_Y3) <T00244486@mymtu.ie>
2025-10-15 14:58:55 +0100

    CM - orders, cart, checkout, order details ui fixed

--- Thryft/Thryft/Program.cs
+++ Thryft/Thryft/Program.cs
@@ -44,7 +44,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+    //app.UseWebAssemblyDebugging();
 }
 else
 {
