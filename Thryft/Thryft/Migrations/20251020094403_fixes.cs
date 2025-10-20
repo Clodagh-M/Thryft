929bc0847db88b5235ec188544e479a8c38a2019
StudentClodaghMulvih <StudentClodaghMulvih@S306-KY-F48V894>
2025-10-20 10:50:45 +0100

    CM -DB

---
+++ Thryft/Thryft/Migrations/20251020094403_fixes.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thryft.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_Users_UserId",
+                table: "Address");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Address",
+                table: "Address");
+
+            migrationBuilder.RenameTable(
+                name: "Address",
+                newName: "Addresses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Address_UserId",
+                table: "Addresses",
+                newName: "IX_Addresses_UserId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Users_UserId",
+                table: "Addresses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Users_UserId",
+                table: "Addresses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "Address");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Addresses_UserId",
+                table: "Address",
+                newName: "IX_Address_UserId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Address",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Address",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Address",
+                table: "Address",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_Users_UserId",
+                table: "Address",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
