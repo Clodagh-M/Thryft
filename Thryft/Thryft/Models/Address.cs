42dfe398fd391be8827631c767956706960465d8
StudentKarenAmumpair <StudentKarenAmumpair@S306-KY-258V894>
2025-10-20 10:36:48 +0100

    ka

---
+++ Thryft/Thryft/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿namespace Thryft.Models;
+
+public class Address
+{
+    public int AddressId { get; set; }
+    public int UserId { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string AddressLine1 { get; set; } = string.Empty;
+    public string? AddressLine2 { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string County { get; set; } = string.Empty;
+    public string Eircode { get; set; } = string.Empty;
+    public bool IsDefault { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation property
+    public User User { get; set; }
+}
