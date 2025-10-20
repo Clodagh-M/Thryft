62ce1572474ea8fa3937cc368f95b1ce1fef29fc
StudentKarenAmumpair <StudentKarenAmumpair@S306-KY-258V894>
2025-10-20 11:02:21 +0100

    KA

---
+++ Thryft/Thryft/Migrations/20251020095226_fixeDataTime.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Thryft.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixeDataTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "GETDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+        }
+    }
+}
