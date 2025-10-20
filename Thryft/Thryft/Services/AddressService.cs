7a1b6ee73902e373d3a626c14ee3b9be4ae400d3
StudentClodaghMulvih <StudentClodaghMulvih@S306-KY-F48V894>
2025-10-20 11:00:00 +0100

    CM - Profile Address

---
+++ Thryft/Thryft/Services/AddressService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Thryft.Data;
+using Thryft.Models;
+
+namespace Thryft.Services;
+
+public class AddressService
+{
+    private readonly IDbContextFactory<AppDbContext> _contextFactory;
+
+    public async Task AddAddressAsync(Address address)
+    {
+        using var context = _contextFactory.CreateDbContext();
+        context.Addresses.Add(address);
+        await context.SaveChangesAsync();
+    }
+
+}
